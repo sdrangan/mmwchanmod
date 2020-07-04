147960d656440acf8034189840924da255fd9bf2
Sundeep Rangan <sdrangan@gmail.com>
2020-07-04 17:36:47 +0200

    Corrected the plot file.

--- plot_los_prob.py
+++ plot_los_prob.py
@@ -12,7 +12,6 @@ tfkl = tf.keras.layers
 import tensorflow.keras.backend as K
 
 from models import ChanMod
-from sklearn.neighbors import KNeighborsClassifier
 
 
 # Load the data
@@ -27,34 +26,57 @@ chan_mod = ChanMod()
 # Load the link classifier model
 chan_mod.load_link_model()
     
-cell_types = [ChanMod.aerial_cell, ChanMod.terr_cell]
+cell_types = [ChanMod.aerial_cell,ChanMod.terr_cell]
 title = ['Aerial', 'Terrestrial']
 nplot = len(cell_types)
-link_state = ChanMod.los_link
+ls0 = ChanMod.los_link
 
 for i, cell_type0 in enumerate(cell_types):
-    """
-    Train a non-parametric classifier
-    """
+    
  
+    """
+    Compute the histogram on the test data
+    """
+    # Set the limits
+    xlim = np.array([0,500])
+    if cell_type0 == ChanMod.aerial_cell:
+        zlim = np.array([-80,130])        
+    else:
+        zlim = np.array([0,130])    
+    
     # Get the test data vector
     dat = test_data
     dvec = dat['dvec']
     dx = np.sqrt(dvec[:,0]**2 + dvec[:,1]**2)
     dz = dvec[:,2]
-    xscale = 0.2
+    
 
     # Get the link state
     ls_ts = chan_mod.get_link_state(dat['los_exists'], dat['nlos_pl'])
     
-    # Create a classifier
-    clf = KNeighborsClassifier(20)
+    # Extract the correct points    
+    I = np.where(dat['cell_type'] == cell_type0)[0]
+    I0 = np.where((dat['cell_type'] == cell_type0) & (ls_ts==ls0))[0]
     
-    # Fit the classifier    
-    X = np.column_stack((dz,xscale*dx))
-    clf.fit(X,ls_ts)
     
-  
+    # Compute the empirical probability
+    H0, xedges, zedges = np.histogram2d(dx[I0],dz[I0],bins=[10,5],range=[xlim,zlim])
+    Htot, xedges, zedges = np.histogram2d(dx[I],dz[I],bins=[10,5],range=[xlim,zlim])
+    prob_ts = H0 / np.maximum(Htot,1)
+    prob_ts = np.flipud(prob_ts.T)
+    
+    # Plot the results
+    plt.subplot(2,2,2*i+1)
+    plt.imshow(prob_ts,aspect='auto',\
+               extent=[np.min(xedges),np.max(xedges),np.min(zedges),np.max(zedges)],\
+               vmin=0, vmax=1)
+    plt.title(title[i] + ' Data')
+    plt.ylabel('Elev (m)')
+    if (i == 0):
+        plt.xticks([])
+    else:
+        plt.xlabel('Horiz (m)')        
+      
 
     """
     Plot the learned model
@@ -63,47 +85,35 @@ for i, cell_type0 in enumerate(cell_types):
     nx = 40
     nz = 20
 
-    if cell_type0 == ChanMod.aerial_cell:
-        zlim = np.array([-80,130])        
-    else:
-        zlim = np.array([-10,130])
-    dx = np.linspace(1,500,nx)        
+    
+    dx = np.linspace(xlim[0],xlim[1],nx)        
     dz = np.linspace(zlim[0],zlim[1],nz)
     dxmat, dzmat = np.meshgrid(dx,dz)
     
-    for j in range(2):
-
-        if j==1:    
-            X = np.column_stack((dzmat.ravel(), xscale*dxmat.ravel()))
-            prob = clf.predict_proba(X)
+    
+    # Run the model to predict the probabilities
+    dvec = np.zeros((nx*nz,3))
+    dvec[:,0] = dxmat.ravel()
+    dvec[:,2] = dzmat.ravel()
+    cell_type = np.tile(cell_type0, (nx*nz,))
+    prob = chan_mod.link_predict(dvec, cell_type)
         
-        else:    
-            # Run the model to predict the probabilities
-            dvec = np.zeros((nx*nz,3))
-            dvec[:,0] = dxmat.ravel()
-            dvec[:,2] = dzmat.ravel()
-            cell_type = np.tile(cell_type0, (nx*nz,))
-            prob = chan_mod.link_predict(dvec, cell_type)
-        #psum = (prob[:,1]+prob[:,2])
-        #prob = prob/psum[:,None]
-        
-        # Plot the probabilties
-        prob = prob.reshape((nz,nx,ChanMod.nlink_states))
-        plt.subplot(2,2,i+2*j+1)
-        plt.imshow(prob[:,:,link_state],\
-                   extent=[np.min(dz),np.max(dz),np.min(dx),np.max(dx)],\
-                   aspect='auto', vmin=0, vmax=1)   
-        plt.xlim(zlim[0], zlim[1])
-        if (i > 0):
-            plt.yticks([])
-        else:
-            plt.ylabel('Horiz (m)')
+    # Plot the probabilties
+    prob = prob.reshape((nz,nx,ChanMod.nlink_states))
+    prob = np.flipud(prob[:,:,ls0])
+    plt.subplot(2,2,2*i+2)
+    plt.imshow(prob,\
+               extent=[np.min(dx),np.max(dx),np.min(dz),np.max(dz)],\
+               aspect='auto', vmin=0, vmax=1)   
+    plt.xlim(xlim[0], xlim[1])
+    if (i ==  0):
+        plt.xticks([])
+    else:
+        plt.xlabel('Horiz (m)')
             
-        # Print the title
-        if j == 0:
-            plt.title(title[i] + ' NN')
-        else:
-            plt.title(title[i] + ' KNN')
+    # Print the title
+    plt.title(title[i] + ' Model')
+            
 
 
 
@@ -113,4 +123,4 @@ if 1:
     cax = plt.axes([0.92, 0.1, 0.05, 0.8])
     plt.colorbar(cax=cax)
 
-
+plt.savefig('los_prob.png', bbox_inches='tight')
